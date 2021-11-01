b8b9ef276a89565738360411f99f322a5cfe7584
haukeeckel <Haukeeckel@posteo.de>
2021-11-01 13:14:06 +0100

    fixes #14 refactoring movement and animation

--- src/classes/Player.js
+++ src/classes/Player.js
@@ -3,112 +3,121 @@ class Player extends GameObject {
     super(config);
     this.isPlayer = true;
     this.toMove = 0;
-    this.input = null;
-    this.viewDirection = {
-      MoveUp: [
-        [0, 1],
-        [3, 0],
-        [3, 1],
-        [3, 2],
-        [3, 3],
-        [3, 4],
-        [3, 5],
-        [0, 1],
-      ],
-      MoveRight: [
-        [0, 0],
-        [4, 0],
-        [4, 1],
-        [4, 2],
-        [4, 3],
-        [4, 4],
-        [4, 5],
-        [0, 0],
-      ],
-      MoveDown: [
-        [0, 3],
-        [1, 0],
-        [1, 1],
-        [1, 2],
-        [1, 3],
-        [1, 4],
-        [1, 5],
-        [0, 3],
-      ],
-      MoveLeft: [
-        [0, 2],
-        [2, 0],
-        [2, 1],
-        [2, 2],
-        [2, 3],
-        [2, 4],
-        [2, 5],
-        [0, 1],
-      ],
-    };
-    this.movement = {
-      MoveUp: ["y", -2],
-      MoveRight: ["x", 2],
-      MoveDown: ["y", 2],
-      MoveLeft: ["x", -2],
+    this.pressedKey = null;
+    this.controls = {
+      ArrowUp: {
+        animations: [
+          [0, 1],
+          [3, 0],
+          [3, 1],
+          [3, 2],
+          [3, 3],
+          [3, 4],
+          [3, 5],
+          [0, 1],
+        ],
+        movement: ["y", -2],
+      },
+      ArrowRight: {
+        animations: [
+          [0, 0],
+          [4, 0],
+          [4, 1],
+          [4, 2],
+          [4, 3],
+          [4, 4],
+          [4, 5],
+          [0, 0],
+        ],
+        movement: ["x", 2],
+      },
+      ArrowDown: {
+        animations: [
+          [0, 3],
+          [1, 0],
+          [1, 1],
+          [1, 2],
+          [1, 3],
+          [1, 4],
+          [1, 5],
+          [0, 3],
+        ],
+        movement: ["y", 2],
+      },
+      ArrowLeft: {
+        animations: [
+          [0, 2],
+          [2, 0],
+          [2, 1],
+          [2, 2],
+          [2, 3],
+          [2, 4],
+          [2, 5],
+          [0, 1],
+        ],
+        movement: ["x", -2],
+      },
     };
     this.blockedGrids = config.blockedGrids;
   }
 
   move() {
+    if (this.toMove === 0 && !pressedKeys.lenght) {
+      this.pressedKey = pressedKeys[0];
+    }
     // fires every Frame
-    this.moveInGrid();
     if (!this.isColliding()) {
       // if the Player is finished Moving and presses a key
       if (this.toMove === 0 && this.validKey()) {
         // set Movement to one Grid
         this.toMove = adjustGrid(1);
-        // set this.input to mapped Controls Value (main.js)
-        this.input = controls[pressedKeys[0]];
-        // set this.srcX /srcY to view direction
       }
     }
+    this.moveInGrid();
   }
 
   // drawing to Grid
   moveInGrid() {
     // if the Player has toMove, change this.x/y by 1/-1
     if (this.toMove > 0) {
-      const axis = this.movement[this.input][0];
-      const value = this.movement[this.input][1];
+      const axis = this.controls[this.pressedKey].movement[0];
+      const value = this.controls[this.pressedKey].movement[1];
+
       // this.x or this.y + 1 or +-1
       this[axis] += value;
-      this.animate()
+      this.animate();
       // reduce toMove by one, untill he's finished Moving
       this.toMove -= 2;
+    } else {
+      this.pressedKey = null;
     }
   }
 
   animate() {
     if (this.toMove > 14) {
-      this.srcX = this.viewDirection[this.input][0][1];
-      this.srcY = this.viewDirection[this.input][0][0] * 2;
+      this.srcX = this.controls[this.pressedKey].animations[0][1];
+      this.srcY = this.controls[this.pressedKey].animations[0][0] * 2;
     } else if (this.toMove > 12) {
-      this.srcX = this.viewDirection[this.input][1][1];
-      this.srcY = this.viewDirection[this.input][1][0] * 2;
+      this.srcX = this.controls[this.pressedKey].animations[1][1];
+      this.srcY = this.controls[this.pressedKey].animations[1][0] * 2;
     } else if (this.toMove > 10) {
-      this.srcX = this.viewDirection[this.input][2][1];
-      this.srcY = this.viewDirection[this.input][2][0] * 2;
+      this.srcX = this.controls[this.pressedKey].animations[2][1];
+      this.srcY = this.controls[this.pressedKey].animations[2][0] * 2;
     } else if (this.toMove > 8) {
-      this.srcX = this.viewDirection[this.input][3][1];
-      this.srcY = this.viewDirection[this.input][3][0] * 2;
+      this.srcX = this.controls[this.pressedKey].animations[3][1];
+      this.srcY = this.controls[this.pressedKey].animations[3][0] * 2;
     } else if (this.toMove > 6) {
-      this.srcX = this.viewDirection[this.input][4][1];
-      this.srcY = this.viewDirection[this.input][4][0] * 2;
+      this.srcX = this.controls[this.pressedKey].animations[4][1];
+      this.srcY = this.controls[this.pressedKey].animations[4][0] * 2;
     } else if (this.toMove > 4) {
-      this.srcX = this.viewDirection[this.input][5][1];
-      this.srcY = this.viewDirection[this.input][5][0] * 2;
+      this.srcX = this.controls[this.pressedKey].animations[5][1];
+      this.srcY = this.controls[this.pressedKey].animations[5][0] * 2;
     } else if (this.toMove > 2) {
-      this.srcX = this.viewDirection[this.input][6][1];
-      this.srcY = this.viewDirection[this.input][6][0] * 2;
+      this.srcX = this.controls[this.pressedKey].animations[6][1];
+      this.srcY = this.controls[this.pressedKey].animations[6][0] * 2;
     } else {
-      this.srcX = this.viewDirection[this.input][0][1];
-      this.srcY = this.viewDirection[this.input][0][0] * 2;
+      this.srcX = this.controls[this.pressedKey].animations[0][1];
+      this.srcY = this.controls[this.pressedKey].animations[0][0] * 2;
     }
   }
 
@@ -118,8 +127,8 @@ class Player extends GameObject {
 
     if (this.validKey()) {
       // Get the next aimed Axis and Value
-      let testAxis = this.movement[controls[pressedKeys[0]]][0];
-      let testValue = this.movement[controls[pressedKeys[0]]][1] / 2;
+      let testAxis = this.controls[this.pressedKey].movement[0];
+      let testValue = this.controls[this.pressedKey].movement[1] / 2;
 
       if (testAxis == "x") {
         let newX = this.x / 16 + testValue;
@@ -154,6 +163,6 @@ class Player extends GameObject {
   }
 
   validKey() {
-    return controllsArray.includes(pressedKeys[0]);
+    return Object.keys(this.controls).includes(this.pressedKey);
   }
 }
--- src/helper.js
+++ src/helper.js
@@ -2,15 +2,3 @@
 const adjustGrid = (field) => {
   return field * 16;
 };
-
-const controls = {
-  ArrowUp: "MoveUp",
-  ArrowRight: "MoveRight",
-  ArrowDown: "MoveDown",
-  ArrowLeft: "MoveLeft",
-};
-
-const controllsArray = Object.keys(controls);
-
-
-
